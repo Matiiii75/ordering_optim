e572cde4e9b5697e8de2851f7b07c87c8d963d92
Matilin Periat <matii@macbook-pro-de-matilin2.home>
2026-09-15 00:44:46 +0200

    ajt trucs

--- src/milp/DSC3.hpp
+++ src/milp/DSC3.hpp
@@ -2,6 +2,14 @@
 
 #include "Milp.hpp"
 
+/*
+implémentation d'un modèle PLNE résolvant DagSumCut. 
+Ce modèle est basé sur [David Coudert, "A note on Integer Linear Programming formulations for linear ordering problems on graphs"]
+Il utilise les variables suivantes : 
+- PHI(u) >= 0 -> capture le nombre de positions où participe u \in V
+- xui = 1 si phi(u) <= i
+*/
+
 struct DSC3 : Milp
 {   
     DSC3(const Data& _data, double _time_limit)
