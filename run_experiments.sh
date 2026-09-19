79d81ccf45754fbcd1e27d96a9aeee8e9ee6028f
Matilin Periat <matii@macbook-pro-de-matilin.home>
2026-09-19 21:31:52 +0200

    modifications sh

--- run_experiments.sh
+++ run_experiments.sh
@@ -84,11 +84,11 @@ run_one_instance() {
 
     # Filtrage selon la logique de ton ancien script (FPT <= 50, MILP <= 500)
     if (( CHOICE_ALGO == 0 || CHOICE_ALGO == 1 || CHOICE_ALGO == 7 || CHOICE_ALGO == 8)); then
-        if (( n < 1000 )); then # ignorer les instances n < 1000
+        if (( n < 5000 )); then # ignorer les instances n < 1000
             return 0
         fi
     else
-        if (( n > 500 )); then
+        if (( n > 250 )); then
             return 0
         fi
     fi
